6d2797e8eb724665812d6b629fce21023d679408
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-08-17 22:26:29 -0300

    Ex4

--- Atividades/Atividade 2/ex4.c
+++ Atividades/Atividade 2/ex4.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
+    int total_voters, white_votes, null_votes, valid_votes;
+    float white_percentual, null_percentual, valid_percentual;
+
+    printf("Digite o número total de eleitores do município: ");
+    scanf("%d", &total_voters);
+
+    printf("Digite o número de votos brancos: ");
+    scanf("%d", &white_votes);
+
+    printf("Digite o número de votos nulos: ");
+    scanf("%d", &null_votes);
+
+    printf("Digite o número de votos válidos: ");
+    scanf("%d", &valid_votes);
+
+    white_percentual = (white_votes / (float)total_voters) * 100;
+    null_percentual = (null_votes / (float)total_voters) * 100;
+    valid_percentual = (valid_votes / (float)total_voters) * 100;
+
+    printf("\nPercentual de votos Brancos: %.2f%%\n", white_percentual);
+    printf("Percentual de votos Nulos: %.2f%%\n", null_percentual);
+    printf("Percentual de votos Válidos: %.2f%%\n", valid_percentual);
 
     return 0;
 }
