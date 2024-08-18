d2136ff059d5b2985b483b694f5a3b5a97ba1053
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-08-17 22:36:31 -0300

    Ex5

---
+++ Atividades/Atividade 2/ex5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float monthly_salary, readjustment_percentage, adjusted_salary;
+
+    printf("Salário Mensal: ");
+    scanf("%f", &monthly_salary);
+
+    printf("Percental de Reajuste: ");
+    scanf("%f", &readjustment_percentage);
+    
+    adjusted_salary = monthly_salary + (monthly_salary * readjustment_percentage / 100);
+    
+    printf("Salário Reajustado: R$%.2f\n", adjusted_salary);
+
+    return 0;
+}
